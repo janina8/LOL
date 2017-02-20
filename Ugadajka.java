a2b928caaf1b3bde60fbe85b78fbd2642fe58bf4
janina8 <j.jancevska@gmail.com>
2017-02-20 20:20:04 +0200

    Create Ugadajka.java

---
+++ Ugadajka.java
@@ -0,0 +1,43 @@
+import java.util.Random; 
+import java.util.Scanner; 
+
+public class Ugadajka {  
+
+        public static void main(String[] args) {
+
+            Random rnd = new Random();
+            Scanner scanner = new Scanner(System.in);
+            int min = 0, max = 10, guess;
+            String sravnenie;
+
+            System.out.println("Загадай число от 1 до 10 ");
+
+            do {
+                guess = rnd.nextInt(max) + min + 1;
+                System.out.println("Твое число больше меньше или равно: " + guess + " ?");
+                sravnenie = scanner.nextLine();
+
+                while(!(sravnenie.equals(">") || sravnenie.equals("<") || sravnenie.equals("="))) {
+                    System.out.println("Ошибка ввода! Повторите попытку.\n");
+                    sravnenie = scanner.nextLine();
+                }
+
+                if (sravnenie.equals(">")) {
+                    System.out.println("Твое число меньше");
+                    max = guess - 1;
+                }
+
+                else  if (sravnenie.equals ("<") ) {
+                    System.out.println("Твое число больше!");
+                    min =  guess + 1 ;
+                    max = 10 ;
+                }
+
+                else if (sravnenie.equals ("=") ) { // тут теперь можно просто елс оставить.
+                    System.out.println("Твое число равно");
+                    break;
+                }
+
+            } while (true);
+        }
+    }

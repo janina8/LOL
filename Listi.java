5a135d9d4ddf402de8a23adbbd1f1e136301d428
janina8 <j.jancevska@gmail.com>
2017-03-20 22:13:54 +0200

    Update Listi.java

--- Listi.java
+++ Listi.java
@@ -8,34 +8,34 @@ import java.util.List;
 public class Listi {
     public static void main(String[] args) throws Exception {
             public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
-        ArrayList<Integer> list3 = new ArrayList<Integer>();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> list1 = new ArrayList<Integer>();
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		ArrayList<Integer> list3 = new ArrayList<Integer>();
 
-        for (int i = 0; i < 20; i++) {
-            
-            int x = Integer.parseInt(reader.readLine());
+		for (int i = 0; i < 20; i++) {
 
-	        if (x % 2 == 0) {
-	            list1.add(x);
-	           
-	        } 
-	        else if (x % 3 == 0) {
-	            list2.add(x);
-	          
-	        }
-	        if (x % 2 == 0 && x % 3 == 0) {
-	            list3.add(x);
-	          
-        }
-	      
-    }
-        printList(list1);
-        printList(list2);
-        printList(list3);
+		    int x = Integer.parseInt(reader.readLine());
 
-    }
+			if (x % 2 == 0) {
+			    list1.add(x);
+
+			} 
+			else if (x % 3 == 0) {
+			    list2.add(x);
+
+			}
+			if (x % 2 == 0 && x % 3 == 0) {
+			    list3.add(x);
+
+		}
+
+	    }
+		printList(list1);
+		printList(list2);
+		printList(list3);
+
+	    }
 	
 	public static void printList(ArrayList list) {
 		System.out.println(list);
